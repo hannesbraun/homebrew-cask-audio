1cd049db5fbe9c2d83ab0911b4fa5efc78a8b494
Hannes Braun <hannesbraun@mail.de>
2019-12-08 22:07:59 +0100

    Update fbvc from 1.0.1 to 1.0.2

--- Casks/fbvc.rb
+++ Casks/fbvc.rb
@@ -1,6 +1,6 @@
 cask 'fbvc' do
-  version '1.0.1'
-  sha256 '6f67a0affc26339c1f71ee8d5a42a8c5f3312716ef5a459e2e7e14067cf820aa'
+  version '1.0.2'
+  sha256 '5349c8c3b155136161b174d7f99d8f1f1fbe639ef1dfc31e3536c1c11fc21c1b'
 
   url "https://www.fullbucket.de/music/dl/fbvc_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/fbvc.html'
