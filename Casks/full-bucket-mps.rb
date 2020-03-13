dfebe9899c835e98bdbcd8ee51d3eadd3ebfacb8
Hannes Braun <hannesbraun@mail.de>
2020-03-13 22:35:47 +0100

    Update full-bucket-mps from 1.1.2 to 1.1.3

--- Casks/full-bucket-mps.rb
+++ Casks/full-bucket-mps.rb
@@ -1,6 +1,6 @@
 cask 'full-bucket-mps' do
-  version '1.1.2'
-  sha256 '4be70bfd4b70e9d8b95675c8a45f930f379eded5ffe3a2de449e37a5a77bd53b'
+  version '1.1.3'
+  sha256 '1b9d766322aac1a3b6272ee577085f54b1ede3ec7517a137915ffc53b2dedde0'
 
   url "https://www.fullbucket.de/music/dl/mps_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/mps.html'
