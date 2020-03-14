844d05d98969d1da201a955a0f49e73cb46b8c0c
Hannes Braun <hannesbraun@mail.de>
2020-03-14 16:06:31 +0100

    Add tal-reverb3 2.2.2

---
+++ Casks/tal-reverb3.rb
@@ -0,0 +1,17 @@
+cask 'tal-reverb3' do
+  version '2.2.2'
+  sha256 'b410ff326d8696142071eb83e013085e32aed864498dfe68a98908a7ddcdff2e'
+
+  url 'https://tal-software.com//downloads/plugins/TAL-reverb-3-installer.pkg'
+  name 'TAL-Reverb-3'
+  homepage 'https://tal-software.com/products/tal-reverb'
+
+  pkg 'TAL-reverb-3-installer.pkg'
+
+  uninstall pkgutil: [
+                       'com.talsoftware.pkg.TAL-Reverb-3-AAX',
+                       'com.talsoftware.pkg.TAL-Reverb-3-VST3',
+                       'com.talsoftware.pkg.TAL-Reverb-3-VST',
+                       'com.talsoftware.pkg.TAL-Reverb-3-AU',
+                     ]
+end
