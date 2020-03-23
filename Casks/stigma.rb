a96373b7ef26a59010126c36ae68ab13e2224bd1
Hannes Braun <hannesbraun@mail.de>
2020-03-23 23:22:57 +0100

    Update stigma from 1.1.2 to 1.2.0

--- Casks/stigma.rb
+++ Casks/stigma.rb
@@ -1,6 +1,6 @@
 cask 'stigma' do
-  version '1.1.2'
-  sha256 'f6cf2eb71be8da19fc8abd7b895779f7b87cb9154e0bc360cb1a80cdf5cbf601'
+  version '1.2.0'
+  sha256 'be616876fff03f195961984020e14a173c444760a4e0cbb036a9651e158c438a'
 
   url "https://www.fullbucket.de/music/dl/stigma_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/stigma.html'
