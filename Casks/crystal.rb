d44cd1058b0023713a76d063b504bcaad22bf22f
Hannes Braun <hannesbraun@mail.de>
2020-03-25 20:29:50 +0100

    Add crystal 2.5.6

---
+++ Casks/crystal.rb
@@ -0,0 +1,17 @@
+cask 'crystal' do
+  version '2.5.6'
+  sha256 'b1800b9a900b4994a11cc88ff2d5dd5cddd9447a92e2c04ce38732a434709781'
+
+  url 'https://www.greenoak.com/crystal/dl/Crystal.dmg'
+  appcast 'https://www.greenoak.com/crystal/dnld2.html',
+          configuration: version.major_minor
+  name 'Crystal'
+  homepage 'https://www.greenoak.com/crystal/dnld2.html'
+
+  pkg 'Crystal_Software.pkg'
+
+  uninstall pkgutil: [
+                       'com.greenoak.Crystal',
+                       'com.greenoakvst.Crystal',
+                     ]
+end
