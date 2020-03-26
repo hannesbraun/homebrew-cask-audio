e510b3283a4423f9ec2047c10a19d1ba0017eb8f
Hannes Braun <hannesbraun@mail.de>
2020-03-26 10:34:20 +0100

    Add ts999 1.5.2

---
+++ Casks/ts999.rb
@@ -0,0 +1,13 @@
+cask 'ts999' do
+  version '1.5.2'
+  sha256 '63d0319767c133ee39d3dc8bb528965eb9f9cd9f42c89ec5bb7b9c1914806fa0'
+
+  # static.kvraudio.com was verified as official when first introduced to the cask
+  url "https://static.kvraudio.com/files/1546/ignite_amps_ts-999_#{version.dots_to_underscores}_mac.zip"
+  appcast 'https://www.kvraudio.com/product/ts-999-by-ignite-amps/'
+  name 'TS-999 SubScreamer'
+  homepage 'http://www.igniteamps.com/'
+
+  audio_unit_plugin 'AU/TS-999.component'
+  vst_plugin 'VST/TS-999.vst'
+end
