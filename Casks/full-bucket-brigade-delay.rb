78937ae493a66cdb1e8c9467b0c534b29a544f82
Hannes Braun <hannesbraun@mail.de>
2020-04-05 01:38:12 +0200

    Update full-bucket-brigade-delay from 1.2.1 to 1.2.2

--- Casks/full-bucket-brigade-delay.rb
+++ Casks/full-bucket-brigade-delay.rb
@@ -1,6 +1,6 @@
 cask 'full-bucket-brigade-delay' do
-  version '1.2.1'
-  sha256 '63d19dccc950270370aae3eabfcbbffdf56514102733d09e0f196bfb05053450'
+  version '1.2.2'
+  sha256 '14864344dd19fde219270dd54053abf10c4e83421e3a3db6cf00d148af102167'
 
   url "https://www.fullbucket.de/music/dl/fbdelay_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/fbdelay.html'
