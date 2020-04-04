bddb3103e6ce57b4c5435d1c754a1410a1218b52
Hannes Braun <hannesbraun@mail.de>
2020-04-05 01:37:37 +0200

    Update tricent from 1.0.4 to 1.0.5

--- Casks/tricent.rb
+++ Casks/tricent.rb
@@ -1,6 +1,6 @@
 cask 'tricent' do
-  version '1.0.4'
-  sha256 '19c5c2729a5f7832ed51752a30f71b5e6ac84fceed3cd24225b1084bdae571de'
+  version '1.0.5'
+  sha256 '3d3b8c28f9b3f610efa96823bce3ebb033022c8dcba71f12588c2c705239ae84'
 
   url "https://www.fullbucket.de/music/dl/tricent_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/tricent.html'
