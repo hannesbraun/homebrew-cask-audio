eb4af9cc58aaab409c90ffd1c048c2c4b31376ed
Hannes Braun <hannesbraun@mail.de>
2020-04-05 12:44:10 +0200

    Update fb3300 from 1.0.6 to 1.1.0

--- Casks/fb3300.rb
+++ Casks/fb3300.rb
@@ -1,6 +1,6 @@
 cask 'fb3300' do
-  version '1.0.6'
-  sha256 '98440e205c39d7b5599447c1ecd54f0fb35461ab3d84ec596c0fedf42efd2b03'
+  version '1.1.0'
+  sha256 'f832b9278352908b5f2190153f168a81e726afdd5a2d24fe668ce6647eedef26'
 
   url "https://www.fullbucket.de/music/dl/fb3300_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/fb3300.html'
@@ -10,8 +10,8 @@ cask 'fb3300' do
   pkg "fb3300_#{version.dots_to_underscores}_mac.pkg"
 
   uninstall pkgutil: [
-                       'com.fullbucket.audiounit.pkg.FB3300',
-                       'com.fullbucket.vst.pkg.FB3300',
+                       'de.fullbucket.audiounit.pkg.FB3300',
+                       'de.fullbucket.vst.pkg.FB3300',
                      ]
 
   caveats do
