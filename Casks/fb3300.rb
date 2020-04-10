dd0b01995c7be8dabc19684d151d76c0431629e8
Hannes Braun <hannesbraun@mail.de>
2020-04-10 14:31:07 +0200

    Update fb3300 from 1.1.0 to 1.1.1

--- Casks/fb3300.rb
+++ Casks/fb3300.rb
@@ -1,6 +1,6 @@
 cask 'fb3300' do
-  version '1.1.0'
-  sha256 'f832b9278352908b5f2190153f168a81e726afdd5a2d24fe668ce6647eedef26'
+  version '1.1.1'
+  sha256 '3f53a9547bb458cf60dc81051caa0328feb1bb7571b213c25a3b4419b1204a1b'
 
   url "https://www.fullbucket.de/music/dl/fb3300_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/fb3300.html'
