97fe4fcc02d099fb6ce519f51d779d4525a6df26
Hannes Braun <hannesbraun@mail.de>
2020-05-14 12:10:52 +0200

    Add delta-modulator 1.01

---
+++ Casks/delta-modulator.rb
@@ -0,0 +1,16 @@
+cask 'delta-modulator' do
+  version '1.01'
+  sha256 '18d73159ad78c1018131c38fc4965651ec5da91ddd953d72fe899d4786502a5b'
+
+  url 'https://xferrecords.com/product_downloads/26/freeware'
+  appcast 'https://xferrecords.com/freeware/'
+  name 'Delta Modulator'
+  homepage 'https://xferrecords.com/freeware/'
+
+  pkg 'Install_Xfer_DeltaModulator.pkg'
+
+  uninstall pkgutil: [
+                       'com.xfer.DeltaModulator',
+                       'com.xfer.DeltaModulator.AU',
+                     ]
+end
