add95a74eacde29e6d28c6d27b1708e56931aec5
Hannes Braun <hannesbraun@mail.de>
2020-05-17 15:47:33 +0200

    Fix delta-modulator url

--- Casks/delta-modulator.rb
+++ Casks/delta-modulator.rb
@@ -2,7 +2,10 @@ cask 'delta-modulator' do
   version '1.01'
   sha256 '18d73159ad78c1018131c38fc4965651ec5da91ddd953d72fe899d4786502a5b'
 
-  url 'https://xferrecords.com/product_downloads/26/freeware'
+  url do
+    require 'open-uri'
+    URI('https://xferrecords.com/product_downloads/26/freeware').open.base_uri.to_s
+  end
   appcast 'https://xferrecords.com/freeware/'
   name 'Delta Modulator'
   homepage 'https://xferrecords.com/freeware/'
