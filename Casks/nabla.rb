6d9f8dae6c026b6bf6f5528be5b2f37f2acbd21e
Hannes Braun <hannesbraun@mail.de>
2020-06-05 16:06:12 +0200

    Update nabla to 1.2.3

--- Casks/nabla.rb
+++ Casks/nabla.rb
@@ -1,6 +1,6 @@
 cask 'nabla' do
-  version '1.2.2'
-  sha256 '7ae993b2e07ecf1838f2ecbe179a5909139a254814326c045f10c5beb8712ef6'
+  version '1.2.3'
+  sha256 '1e9b0de42f0ed4dd9f22694dfebf5a5b905e30796d1c905d339e02af42184873'
 
   url "https://www.fullbucket.de/music/dl/nabla_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/nabla.html'
