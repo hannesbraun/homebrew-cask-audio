7c9ba50c37f7e2b638f7fe736e6d1bdfb41e0e94
Hannes Braun <hannesbraun@mail.de>
2020-07-18 10:33:25 +0200

    Update monofury from 2.2.2 to 2.2.3

--- Casks/monofury.rb
+++ Casks/monofury.rb
@@ -1,6 +1,6 @@
 cask 'monofury' do
-  version '2.2.2'
-  sha256 'a82ed7b182356cbc13696e1f71b5523bd00f436a88a5b4453c66d491b88e3b50'
+  version '2.2.3'
+  sha256 '5adc7195d216ca5a5dd2b0b05dba9422a861ce8cd4ca774beb780a2dd989a489'
 
   url "https://www.fullbucket.de/music/dl/monofury_#{version.dots_to_underscores}_mac.pkg"
   appcast 'https://www.fullbucket.de/music/monofury.html'
