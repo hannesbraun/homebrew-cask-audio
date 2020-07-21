dfbab3ae240de961e0c276452bb9baf50039ee09
Hannes Braun <hannesbraun@mail.de>
2020-07-21 20:44:11 +0200

    Update odin2 from 2.1.0 to 2.2.2

--- Casks/odin2.rb
+++ Casks/odin2.rb
@@ -1,10 +1,27 @@
 cask 'odin2' do
-  version '2.1.0'
-  sha256 '7be16bfcfce4e1f8667f57034e35e06fc908b41be7d7d382afe8f9aed3f6495f'
+  version '2.2.2'
+  sha256 '55e01889c2a3f0a808b00ba188f7080b96ea08c09442952e89f4a1469c4dcd7c'
 
-  url "https://www.thewavewarden.com/assets/Odin#{version.major_minor}_mac_x64.zip"
+  url 'https://www.thewavewarden.com/assets/Odin2MacInstaller.pkg'
   name 'Odin 2'
   homepage 'https://www.thewavewarden.com/odin2'
 
-  vst3_plugin 'Odin2.vst3'
+  pkg 'Odin2MacInstaller.pkg',
+      choices: [
+                 {
+                   'choiceIdentifier' => 'installer_choice_1',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'installer_choice_2',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+               ]
+
+  uninstall pkgutil: [
+                       'com.thewavewarden.pkg.odin2AU',
+                       'com.thewavewarden.pkg.odin2VST',
+                     ]
 end
