3cd610147ecf77d4ddb97ddf08e43e1d088b3132
Hannes Braun <hannesbraun@mail.de>
2020-08-18 17:34:07 +0200

    Update pg-8x url and appcast

--- Casks/pg-8x.rb
+++ Casks/pg-8x.rb
@@ -2,8 +2,10 @@ cask "pg-8x" do
   version "2.0.13"
   sha256 "68e49fe103ba69c390229cf2c8bd9353ab7d587bcc5ba036d32ebcf053032bef"
 
-  url "https://sites.google.com/site/mlvst0/pg8x-#{version.major}/PG-8X_#{version}_MacVST.zip"
-  appcast "https://sites.google.com/site/mlvst0/pg8x-#{version.major}"
+  # https://drive.google.com/uc?export=download&id=1zbSGazrU2r0HRH_rskQk1kXRj10nyKFJ was verified as official when first introduced to the cask
+  url "https://drive.google.com/uc?export=download&id=1zbSGazrU2r0HRH_rskQk1kXRj10nyKFJ"
+  appcast "https://sites.google.com/site/mlvst0/pg8x-#{version.major}",
+          must_contain: version.major_minor
   name "PG-8X"
   homepage "https://sites.google.com/site/mlvst0/pg8x-#{version.major}"
 
