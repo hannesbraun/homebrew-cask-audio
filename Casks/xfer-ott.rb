f5106fb508c3f1d70b97152a5d53c6e9d4f848b7
Hannes Braun <hannesbraun@mail.de>
2020-08-18 17:36:30 +0200

    Update xfer-ott shasum

--- Casks/xfer-ott.rb
+++ Casks/xfer-ott.rb
@@ -1,6 +1,6 @@
 cask "xfer-ott" do
   version "1.25"
-  sha256 "4bef138455a9ea8925d75bd04bfa92bac2a3f901d894938b3d9b78c4e443897f"
+  sha256 "458dae3cdd12d72277c1af7d5e5ddb6d386562ecffbf79e7743176ee47913b91"
 
   url "https://xferrecords.com/product_downloads/24/freeware"
   appcast "https://xferrecords.com/freeware/"
