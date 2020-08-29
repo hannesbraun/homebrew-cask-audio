e7be82501026a218016aa2c3ce76f99f98a02200
Hannes Braun <hannesbraun@mail.de>
2020-08-29 22:58:02 +0200

    Fix style for smexoscope

--- Casks/smexoscope.rb
+++ Casks/smexoscope.rb
@@ -8,7 +8,6 @@ cask "smexoscope" do
 
   audio_unit_plugin "smexoscope/macOS/Components/s(M)exoscope.component"
   vst_plugin "smexoscope/macOS/VST/s(M)exoscope.vst"
-
   vst3_plugin "smexoscope/macOS/VST3/s(M)exoscope.vst3"
 
   caveats "This is the 64-bit port of the original s(M)exoscope made by Bram DeJong and Sean Evans."
