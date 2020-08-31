9a8c9930178d4f25645b506ed0320bfbdb7c4214
Hannes Braun <hannesbraun@mail.de>
2020-08-31 10:23:13 +0200

    Update tdr-vos-slickeq from 1.3.3 to 1.3.4

--- Casks/tdr-vos-slickeq.rb
+++ Casks/tdr-vos-slickeq.rb
@@ -1,10 +1,11 @@
 cask "tdr-vos-slickeq" do
-  version "1.3.3"
-  sha256 "e1fe8947ac31015d82ca99365339e6cefa9abd06c1d8c687a9278a7f97dddc79"
+  version "1.3.4"
+  sha256 "b020c84035a846b29312a580c93f10a868c17d60dabd055731dd401e06a653b9"
 
   url "https://www.tokyodawn.net/labs/SlickEQ/release#{version}/TDR%20VOS%20SlickEQ.dmg"
   appcast "https://www.tokyodawn.net/tdr-vos-slickeq/"
   name "TDR VOS SlickEQ"
+  desc "Mixing/mastering equalizer"
   homepage "https://www.tokyodawn.net/tdr-vos-slickeq/"
 
   audio_unit_plugin "TDR VOS SlickEQ.component"
