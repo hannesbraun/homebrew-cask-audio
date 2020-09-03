126e82f9d3c377c8298f8965820b237f8a24753c
Hannes Braun <hannesbraun@mail.de>
2020-09-03 19:15:39 +0200

    Update pteq-x from 1.1.0 to 1.1.1

--- Casks/pteq-x.rb
+++ Casks/pteq-x.rb
@@ -1,11 +1,12 @@
 cask "pteq-x" do
-  version "1.1.0"
-  sha256 "9a74a4ac0f6be019664008c0b96d40399c457ccfbacb9096d00f9ae6fc95a223"
+  version "1.1.1"
+  sha256 "f683482e8c9d5e0d247b776b80f36428cbf79f47534d5e9fce3925f4486e4c44"
 
   # static.kvraudio.com was verified as official when first introduced to the cask
   url "https://static.kvraudio.com/files/1546/ignite_amps_pteq-x_#{version.dots_to_underscores}_mac.zip"
   appcast "https://www.kvraudio.com/product/pteq-x-by-ignite-amps/"
   name "PTEq-X"
+  desc "Digital emulation of 3 famous vintage passive program equalizers"
   homepage "http://www.igniteamps.com/"
 
   audio_unit_plugin "AU/PTEq-X.component"
