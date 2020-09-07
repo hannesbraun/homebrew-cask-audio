b91d23a8a7c71b33349e29b980d9b5741ee9a978
Hannes Braun <hannesbraun@mail.de>
2020-09-07 23:14:08 +0200

    Add oldskoolverb 2.7

---
+++ Casks/oldskoolverb.rb
@@ -0,0 +1,15 @@
+cask "oldskoolverb" do
+  version "2.7"
+  sha256 "6a0a2038848077997f359ed86481426cf4f6cc10ff2d644f13ed439269af4ec1"
+
+  url "https://www.voxengo.com/files/VoxengoOldSkoolVerb_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
+  appcast "https://www.voxengo.com/product/oldskoolverb/",
+          must_contain: version.no_dots
+  name "Voxengo OldSkoolVerb"
+  desc "Classic reverb plugin"
+  homepage "https://www.voxengo.com/product/oldskoolverb/"
+
+  audio_unit_plugin "OldSkoolVerb.component"
+  artifact "ProTools AAX/OldSkoolVerb.aaxplugin",
+           target: "/Library/Application Support/Avid/Audio/Plug-Ins/OldSkoolVerb.aaxplugin"
+end
