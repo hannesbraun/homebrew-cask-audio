d076d961d11ab9201d50b16a7cb107974d6e85d2
Hannes Braun <hannesbraun@mail.de>
2020-11-01 17:22:24 +0100

    Fix transformant

--- Casks/transformant.rb
+++ Casks/transformant.rb
@@ -1,13 +1,13 @@
 cask "transformant" do
   version "1.0.2,13e0ea27fe9a4b557d48829a7f8bd001"
-  sha256 "41dc28e329f452981b7fd2084d64845e8339bd73a3986f2106e79a6478ab75ac"
+  sha256 "4274bd55d077d043819b52d4d24ae821209740263cad1e2061fec8b11e9e0ebe"
 
   url "https://www.igorski.nl/download/retrieve/#{version.after_comma}"
   appcast "https://www.igorski.nl/download/transformant--multichannel-formantic-noise-processor"
   name "Transformant"
   homepage "https://www.igorski.nl/download/transformant--multichannel-formantic-noise-processor"
 
-  audio_unit_plugin "Transformant/macOS/Audio Unit/transformant_au.component"
-  vst_plugin "Transformant/macOS/VST/transformant.vst"
-  vst3_plugin "Transformant/macOS/VST3/transformant.vst3"
+  audio_unit_plugin "macOS/Audio Unit/transformant_au.component"
+  vst_plugin "macOS/VST/transformant.vst"
+  vst3_plugin "macOS/VST3/transformant.vst3"
 end
