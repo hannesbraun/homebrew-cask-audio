1b5b78eb546e99548eb5132fb09e990e39d5acd5
Hannes Braun <hannesbraun@mail.de>
2020-11-01 17:22:03 +0100

    Fix fogpad

--- Casks/fogpad.rb
+++ Casks/fogpad.rb
@@ -1,13 +1,13 @@
 cask "fogpad" do
   version "1.0.2,83b12af28e733df71aaa5621e384d572"
-  sha256 "a04b850d8fb9ae27b362acae5d69add03e7df47bef7550b1dfec68c3e518a80c"
+  sha256 "3d25036b40acb37244994ab727c6a5e5fadee3adfee34ee36f6dfcef7a1b81ca"
 
   url "https://www.igorski.nl/download/retrieve/#{version.after_comma}"
   appcast "https://www.igorski.nl/download/fogpad--reverb-plugin"
   name "FogPad"
   homepage "https://www.igorski.nl/download/fogpad--reverb-plugin"
 
-  audio_unit_plugin "FogPad/macOS/AU/fogpad.component"
-  vst_plugin "FogPad/macOS/VST/fogpad.vst"
-  vst3_plugin "FogPad/macOS/VST3/fogpad.vst3"
+  audio_unit_plugin "macOS/AU/fogpad.component"
+  vst_plugin "macOS/VST/fogpad.vst"
+  vst3_plugin "macOS/VST3/fogpad.vst3"
 end
