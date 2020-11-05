2c5bf5a848108e21b1d73603e4b3bfd2bb91cd36
Hannes Braun <hannesbraun@mail.de>
2020-11-05 09:03:33 +0100

    Add valhallaspacemodulator 1.1.6

---
+++ Casks/valhallaspacemodulator.rb
@@ -0,0 +1,20 @@
+cask "valhallaspacemodulator" do
+  version "1.1.6"
+  sha256 "3edc3c81e75d7f4d5372cd40c7da15f74694f7a16eb9d5ac1795c49edf92c284"
+
+  # valhallaproduction.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
+  url "https://valhallaproduction.s3-us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSXBeta_#{version.dots_to_underscores}.zip"
+  appcast "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
+  name "Valhalla Space Modulator"
+  homepage "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
+
+  pkg "ValhallaSpaceModulatorOSXBeta.pkg"
+
+  uninstall pkgutil: [
+    "com.valhalladsp.SpaceModulator.pkg.aax",
+    "com.valhalladsp.SpaceModulator.pkg.au",
+    "com.valhalladsp.SpaceModulator.pkg.presets",
+    "com.valhalladsp.SpaceModulator.pkg.vst",
+    "com.valhalladsp.SpaceModulator.pkg.vst3",
+  ]
+end
