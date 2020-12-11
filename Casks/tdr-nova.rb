45b79f1a4c9e29b6555911a9fea74814d5ce7d1e
Hannes Braun <hannesbraun@mail.de>
2020-12-11 13:07:43 +0100

    Update tdr-nova from 2.1.2 to 2.1.3

--- Casks/tdr-nova.rb
+++ Casks/tdr-nova.rb
@@ -1,15 +1,19 @@
 cask "tdr-nova" do
-  version "2.1.2"
-  sha256 "3735da3759b953c63b948e002905c901d4e1e16a28ab96f67e234cbece100de7"
+  version "2.1.3"
+  sha256 "368246676adfee2bec51f3335c58c6e8e949f8a1e0c213bc499f60e3d90c2242"
 
-  url "https://www.tokyodawn.net/labs/Nova/#{version}/TDR%20Nova.dmg"
+  url "https://www.tokyodawn.net/labs/Nova/#{version}/TDR%20Nova.zip"
   appcast "https://www.tokyodawn.net/tdr-nova/"
   name "TDR Nova"
   desc "Parallel dynamic equalizer"
   homepage "https://www.tokyodawn.net/tdr-nova/"
 
-  audio_unit_plugin "TDR Nova.component"
-  vst_plugin "TDR Nova.vst"
-  vst3_plugin "TDR Nova.vst3"
-  artifact "TDR Nova.aaxplugin", target: "/Library/Application Support/Avid/Audio/Plug-Ins/TDR Nova.aaxplugin"
+  pkg "TDR Nova.pkg"
+
+  uninstall pkgutil: [
+    "com.TokyoDawnLabs.TDRNova.AAX",
+    "com.TokyoDawnLabs.TDRNova.AU",
+    "com.TokyoDawnLabs.TDRNova.VST",
+    "com.TokyoDawnLabs.TDRNova.VST3",
+  ]
 end
