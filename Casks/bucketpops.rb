859d86922cf99b06bf68bc53b2850b62cf5132fc
Hannes Braun <hannesbraun@mail.de>
2020-12-18 16:46:22 +0100

    Update bucketpops from 1.0.1 to 1.0.2

--- Casks/bucketpops.rb
+++ Casks/bucketpops.rb
@@ -1,6 +1,6 @@
 cask "bucketpops" do
-  version "1.0.1"
-  sha256 "2666cbc13ba2a77e237bc3f6bc60f77be09f15601ba9bc899f278360b3bfd285"
+  version "1.0.2"
+  sha256 "72a9bb64564818a816f07e4fddfdb631d56caa96e33c8549de02f4bcffd9ec41"
 
   url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_mac.pkg"
   appcast "https://www.fullbucket.de/music/bucketpops.html"
