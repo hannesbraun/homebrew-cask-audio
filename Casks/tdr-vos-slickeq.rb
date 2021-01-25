6f78630d7682513adffb7aae917db70368c8f558
Hannes Braun <hannesbraun@mail.de>
2021-01-25 12:40:39 +0100

    Update tdr-vos-slickeq from 1.3.5 to 1.3.6

--- Casks/tdr-vos-slickeq.rb
+++ Casks/tdr-vos-slickeq.rb
@@ -1,6 +1,6 @@
 cask "tdr-vos-slickeq" do
-  version "1.3.5"
-  sha256 "a0507f45740e284f185b19732bce5d6722a9ee204ccae0b258a14a58a4a3fd7b"
+  version "1.3.6"
+  sha256 "cd7e5238e682f163b1f6890cfe8976cf42fb8a9755421d9a7575e060e912f030"
 
   url "https://www.tokyodawn.net/labs/SlickEQ/#{version}/TDR%20VOS%20SlickEQ.zip"
   appcast "https://www.tokyodawn.net/tdr-vos-slickeq/"
