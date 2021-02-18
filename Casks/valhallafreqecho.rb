940583d6a3deb9c8941c270b9a6206daaa876597
Hannes Braun <hannesbraun@mail.de>
2021-02-18 01:52:52 +0100

    Update valhallafreqecho from 1.2.0 to 1.2.5.1

--- Casks/valhallafreqecho.rb
+++ Casks/valhallafreqecho.rb
@@ -1,6 +1,6 @@
 cask "valhallafreqecho" do
-  version "1.2.0"
-  sha256 "e56653c8510802a1e8be66a9807138f4f86527d27eb7df735e5da9e0c9e2310c"
+  version "1.2.5.1"
+  sha256 "87b4f7b5b96e6de93db4cd01044b9427f1ddfb8c6a5953443bc404d43dda8279"
 
   url "https://valhallaproduction.s3-us-west-2.amazonaws.com/freqecho/ValhallaFreqEchoOSX_#{version.dots_to_underscores}.zip",
       verified: "valhallaproduction.s3-us-west-2.amazonaws.com/"
