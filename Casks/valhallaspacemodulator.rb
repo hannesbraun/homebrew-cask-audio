bd446b3bba2d62a0e5dc9dbd5274dc0603187c46
Hannes Braun <hannesbraun@mail.de>
2021-02-18 01:47:47 +0100

    Update valhallspacemodulator from 1.1.6 to 1.2.5

--- Casks/valhallaspacemodulator.rb
+++ Casks/valhallaspacemodulator.rb
@@ -1,14 +1,14 @@
 cask "valhallaspacemodulator" do
-  version "1.1.6"
-  sha256 "3edc3c81e75d7f4d5372cd40c7da15f74694f7a16eb9d5ac1795c49edf92c284"
+  version "1.2.5"
+  sha256 "ccc245caf6c9698aa20a857092f3266ed5496f95baa71d1a5e1bbf8a2d871739"
 
-  url "https://valhallaproduction.s3-us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSXBeta_#{version.dots_to_underscores}.zip",
+  url "https://valhallaproduction.s3-us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSX_#{version.dots_to_underscores}.zip",
       verified: "valhallaproduction.s3-us-west-2.amazonaws.com/"
   appcast "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
   name "Valhalla Space Modulator"
   homepage "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
 
-  pkg "ValhallaSpaceModulatorOSXBeta.pkg"
+  pkg "ValhallaSpaceModulatorOSX.pkg"
 
   uninstall pkgutil: [
     "com.valhalladsp.SpaceModulator.pkg.aax",
