cbc0b61f4147eca2f606f2f42c581ed78e204692
Hannes Braun <hannesbraun@mail.de>
2021-02-22 15:28:18 +0100

    Update djmfilter from 1.09 to 1.11

--- Casks/djmfilter.rb
+++ Casks/djmfilter.rb
@@ -1,6 +1,6 @@
 cask "djmfilter" do
-  version "1.09"
-  sha256 "fccdfc393c418f18b79dd76355a9e9a228fbde7af0b6d132004de558cd475627"
+  version "1.11"
+  sha256 :no_check
 
   url "https://xferrecords.com/product_downloads/68/freeware"
   appcast "https://xferrecords.com/freeware/"
