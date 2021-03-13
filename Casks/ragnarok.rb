b387b0f915d967e2fdac899dab4bf82761fb7f38
Hannes Braun <hannesbraun@mail.de>
2021-03-14 00:09:47 +0100

    ragnarok: add livecheck

--- Casks/ragnarok.rb
+++ Casks/ragnarok.rb
@@ -3,10 +3,15 @@ cask "ragnarok" do
   sha256 "b11f794a4b4e6f7d7bbdc98c2bf2ce8780effc174e652af5fbca3e9f1395201c"
 
   url "https://www.fullbucket.de/music/dl/ragnarok_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/ragnarok.html"
   name "Ragnarök"
   desc "Performance synthesizer"
   homepage "https://www.fullbucket.de/music/ragnarok.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "ragnarok_#{version.dots_to_underscores}_mac.pkg"
 
