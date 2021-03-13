06548a774f732749dd7836348149c6370002060a
Hannes Braun <hannesbraun@mail.de>
2021-03-14 00:09:47 +0100

    tricent: add livecheck

--- Casks/tricent.rb
+++ Casks/tricent.rb
@@ -3,10 +3,15 @@ cask "tricent" do
   sha256 "44edce54522de9c086bb034ce6f6827f3d23ddfeeac3df109f1a70055eec8f5a"
 
   url "https://www.fullbucket.de/music/dl/tricent_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/tricent.html"
   name "Tricent mk III"
   desc "Polyphonic synthesizer ensemble"
   homepage "https://www.fullbucket.de/music/tricent.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "tricent_#{version.dots_to_underscores}_mac.pkg"
 
