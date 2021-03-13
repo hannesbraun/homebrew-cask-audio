37f0de1103e4d944b099553efb2b901cd19f851d
Hannes Braun <hannesbraun@mail.de>
2021-03-14 00:09:46 +0100

    fbvc: add livecheck

--- Casks/fbvc.rb
+++ Casks/fbvc.rb
@@ -3,10 +3,15 @@ cask "fbvc" do
   sha256 "5349c8c3b155136161b174d7f99d8f1f1fbe639ef1dfc31e3536c1c11fc21c1b"
 
   url "https://www.fullbucket.de/music/dl/fbvc_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/fbvc.html"
   name "Full Bucket Vocoder FBVC"
   desc "KORG Vocoder VC-10 simulation"
   homepage "https://www.fullbucket.de/music/fbvc.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "fbvc_#{version.dots_to_underscores}_mac.pkg"
 
