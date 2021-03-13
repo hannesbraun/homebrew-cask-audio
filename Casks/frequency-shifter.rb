496c8e179f935dba57eb4b350c4fc31b74ba9dcd
Hannes Braun <hannesbraun@mail.de>
2021-03-14 00:09:46 +0100

    frequency-shifter: add livecheck

--- Casks/frequency-shifter.rb
+++ Casks/frequency-shifter.rb
@@ -3,10 +3,15 @@ cask "frequency-shifter" do
   sha256 "65cd69c2072f3afc08b761e94ab536af8b0f5a159a6283660543e83d7f62f5df"
 
   url "https://www.fullbucket.de/music/dl/freqshifter_#{version.dots_to_underscores}_mac.pkg"
-  appcast "https://www.fullbucket.de/music/freqshifter.html"
   name "Frequency Shifter"
   desc "Bode frequency shifter"
   homepage "https://www.fullbucket.de/music/freqshifter.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)*)/)
+  end
 
   pkg "freqshifter_#{version.dots_to_underscores}_mac.pkg"
 
