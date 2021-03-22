440667bacbad8c740ae783d33d768247e371701a
Hannes Braun <hannesbraun@mail.de>
2021-03-22 14:07:18 +0100

    tdr-molotok: add livecheck

--- Casks/tdr-molotok.rb
+++ Casks/tdr-molotok.rb
@@ -3,10 +3,15 @@ cask "tdr-molotok" do
   sha256 "7afeb390a221f1fe7018fb55e0c859ab2ca91108a367fac3b71eb1af96eef885"
 
   url "https://www.tokyodawn.net/labs/Molotok/#{version}/TDR%20Molotok.zip"
-  appcast "https://www.tokyodawn.net/tdr-molotok/"
   name "TDR Molotok"
   desc "Characterful dynamics processor"
   homepage "https://www.tokyodawn.net/tdr-molotok/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{(\d+(?:\.\d+)*)/TDR Molotok\.zip})
+  end
 
   pkg "TDR Molotok.pkg"
 
