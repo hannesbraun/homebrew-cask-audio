4bb5061b9f8102fbba0d6fb0071a3a06e6277c3f
Hannes Braun <hannesbraun@mail.de>
2021-03-22 14:07:32 +0100

    tdr-nova: add livecheck

--- Casks/tdr-nova.rb
+++ Casks/tdr-nova.rb
@@ -3,10 +3,15 @@ cask "tdr-nova" do
   sha256 "9cd1b070e08af529d31656c116388d6765be424d230e8bd5347389f2e3a0e54b"
 
   url "https://www.tokyodawn.net/labs/Nova/#{version}/TDR%20Nova.zip"
-  appcast "https://www.tokyodawn.net/tdr-nova/"
   name "TDR Nova"
   desc "Parallel dynamic equalizer"
   homepage "https://www.tokyodawn.net/tdr-nova/"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(%r{(\d+(?:\.\d+)*)/TDR Nova\.zip})
+  end
 
   pkg "TDR Nova.pkg"
 
