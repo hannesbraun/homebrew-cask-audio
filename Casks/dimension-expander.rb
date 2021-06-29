44355163c8c50254693f8503c7c73aca4f985ed5
Hannes Braun <hannesbraun@mail.de>
2021-06-29 15:25:54 +0200

    Update dimension-expander from 1.01 to 1.23

--- Casks/dimension-expander.rb
+++ Casks/dimension-expander.rb
@@ -1,16 +1,17 @@
 cask "dimension-expander" do
-  version "1.01"
-  sha256 "a845850ab6e8f31188c1883cdef98d63f48af94efaf9355d8f01001d6ba9d6cd"
+  version "1.23"
+  sha256 :no_check
 
   url "https://xferrecords.com/product_downloads/32/freeware"
   appcast "https://xferrecords.com/freeware/"
   name "Dimension Expander"
+  desc "Chorus/Spatial Expander"
   homepage "https://xferrecords.com/freeware/"
 
   pkg "Install_Xfer_DimensionExpander.pkg"
 
   uninstall pkgutil: [
-    "com.xfer.DimensionExpander.AU",
-    "com.xfer.DimensionExpander.VST",
+    "com.xfer.DimExp.AU",
+    "com.xfer.DimExp.VST",
   ]
 end
