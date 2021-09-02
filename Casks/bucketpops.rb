5028270f6c73dd5f16f59162b50954b13467b5b1
Hannes Braun <hannesbraun@mail.de>
2021-09-02 15:10:19 +0200

    Update bucketpops from 1.0.3 to 1.0.4

--- Casks/bucketpops.rb
+++ Casks/bucketpops.rb
@@ -1,8 +1,14 @@
 cask "bucketpops" do
-  version "1.0.3"
-  sha256 "b56242010278117f9db9bb4314c4cea56462d443009217af6c6078379bd855b4"
+  version "1.0.4"
 
-  url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_mac.pkg"
+  if MacOS.version <= :mojave
+    sha256 "bdbb0d1500721fcd87edd7e87cec26d9efe0467b5491e5e37beac4ebfc8d3f4a"
+    url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_mac.pkg"
+  else
+    sha256 "18b65fc2c6c3bd9eff8300db7568eb4574077b3006e8ff35b86f5790bfe4c9d9"
+    url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_METAL_mac.pkg"
+  end
+
   name "Bucket Pops"
   desc "KORG Mini Pops-7 simulation"
   homepage "https://www.fullbucket.de/music/bucketpops.html"
