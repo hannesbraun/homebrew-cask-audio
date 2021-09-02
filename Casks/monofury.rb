e11f9addb888d407a03740b514e8a41bc55bd900
Hannes Braun <hannesbraun@mail.de>
2021-09-02 18:47:04 +0200

    monofury: add Metal version for newer Macs

--- Casks/monofury.rb
+++ Casks/monofury.rb
@@ -1,8 +1,14 @@
 cask "monofury" do
   version "2.2.6"
-  sha256 "fc2c0e0fa368dca101beeba5285d56b21a083a9bbc6a082108dd1a21cdb64c0d"
 
-  url "https://www.fullbucket.de/music/dl/monofury_#{version.dots_to_underscores}_mac.pkg"
+  if MacOS.version <= :mojave
+    sha256 "fc2c0e0fa368dca101beeba5285d56b21a083a9bbc6a082108dd1a21cdb64c0d"
+    url "https://www.fullbucket.de/music/dl/monofury_#{version.dots_to_underscores}_mac.pkg"
+  else
+    sha256 "33cf66d72ad3f5fc4080d41f467c46f0f962113cf430b539efd7b7a9127a832a"
+    url "https://www.fullbucket.de/music/dl/monofury_#{version.dots_to_underscores}_METAL_mac.pkg"
+  end
+
   name "Mono/Fury"
   desc "KORG Mono/Poly simulation"
   homepage "https://www.fullbucket.de/music/monofury.html"
@@ -13,7 +19,11 @@ cask "monofury" do
     regex(/v(\d+(?:\.\d+)*)/)
   end
 
-  pkg "monofury_#{version.dots_to_underscores}_mac.pkg"
+  if MacOS.version <= :mojave
+    pkg "monofury_#{version.dots_to_underscores}_mac.pkg"
+  else
+    pkg "monofury_#{version.dots_to_underscores}_METAL_mac.pkg"
+  end
 
   uninstall pkgutil: [
     "de.fullbucket.audiounit.pkg.MonoFury",
