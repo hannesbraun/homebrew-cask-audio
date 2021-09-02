6285bb549ca5acf028c51c60846bc17255964203
Hannes Braun <hannesbraun@mail.de>
2021-09-02 13:13:53 +0200

    Update tdr-kotelnikov from 1.6.3 to 1.6.4

--- Casks/tdr-kotelnikov.rb
+++ Casks/tdr-kotelnikov.rb
@@ -1,6 +1,6 @@
 cask "tdr-kotelnikov" do
-  version "1.6.3"
-  sha256 "d2d551b5c63fd66f38154e10f80c8ab36d516702404810503628a1f12dae778c"
+  version "1.6.4"
+  sha256 "eec5ad8d12ed25bb6501ed5058fa309ddb61d8c8ebf1a1b28c621b4f8b1ba2ee"
 
   url "https://www.tokyodawn.net/labs/Kotelnikov/#{version}/TDR%20Kotelnikov.zip"
   name "TDR Kotelnikov"
