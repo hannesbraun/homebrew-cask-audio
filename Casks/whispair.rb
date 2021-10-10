1814b6ae49506e0d41fef3d35df4ea25bced69a0
Hannes Braun <hannesbraun@mail.de>
2021-10-10 23:29:22 +0200

    Update whispair from 1.0.3 to 1.0.4

--- Casks/whispair.rb
+++ Casks/whispair.rb
@@ -1,11 +1,11 @@
 cask "whispair" do
-  version "1.0.3"
+  version "1.0.4"
 
   if MacOS.version <= :mojave
-    sha256 "aec66c9b220945859095d0a7ceb0f233c7d751544d75ab43fb7a6f024799102f"
+    sha256 "i4109cfb567bfcec0d428e88bc98df7008651129fc5c49183129fe85d5f7bcbec"
     url "https://www.fullbucket.de/music/dl/whispair_#{version.dots_to_underscores}_mac.pkg"
   else
-    sha256 "d42695158dfbad4d37f64f1f08e3405204739346be976db9ef65050f6a8b49e0"
+    sha256 "65acf105074bceea019683821eb6a2ee2237fefcb530ee8815988211ee2958c3"
     url "https://www.fullbucket.de/music/dl/whispair_#{version.dots_to_underscores}_METAL_mac.pkg"
   end
 
