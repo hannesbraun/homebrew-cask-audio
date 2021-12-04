074689d7ad96ddca2b1313f956b7b35550cbc1bd
Hannes Braun <hannesbraun@mail.de>
2021-12-04 15:27:28 +0100

    Update odin2 from 2.2.4 to 2.3.2

--- Casks/odin2.rb
+++ Casks/odin2.rb
@@ -1,13 +1,13 @@
 cask "odin2" do
-  version "2.2.4"
-  sha256 :no_check
+  version "2.3.2"
+  sha256 "607828fcd67b5c3b4c08ca0e885c8bc96c5a0ff9b1e532d3ea81c7f0f9ca106d"
 
-  url "https://www.thewavewarden.com/assets/Odin2MacInstaller.pkg"
+  url "https://www.thewavewarden.com/assets/Odin#{version}MacInstaller.pkg"
   name "Odin 2"
   desc "Synthesizer plugin"
   homepage "https://www.thewavewarden.com/odin2"
 
-  pkg "Odin2MacInstaller.pkg",
+  pkg "Odin#{version}MacInstaller.pkg",
       choices: [
         {
           "choiceIdentifier" => "installer_choice_1",
