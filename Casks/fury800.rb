0f26e9a6599217046d8afc4917480035f1320894
Hannes Braun <hannesbraun@mail.de>
2022-01-17 11:15:31 +0100

    Update fury800 from 2.3.0 to 2.4.0

--- Casks/fury800.rb
+++ Casks/fury800.rb
@@ -1,8 +1,14 @@
 cask "fury800" do
-  version "2.3.0"
-  sha256 "695c19424ff0106dd9b63207ed2e90cdc1cdf3a0b4af9cd0dd32c95dcbba21cf"
+  version "2.4.0"
 
-  url "https://www.fullbucket.de/music/dl/fury800_#{version.dots_to_underscores}_mac.pkg"
+  if MacOS.version <= :mojave
+    sha256 "f9dd2b33876acf10697208b5a44fd968d54e64ce117d1b411ee81e54612eb6d7"
+    url "https://www.fullbucket.de/music/dl/fury800_#{version.dots_to_underscores}_N_mac.pkg"
+  else
+    sha256 "a2c0105dace2a39edc2796a31f65d01adf034c8da4e7e1d60f7e8218b0a946ea"
+    url "https://www.fullbucket.de/music/dl/fury800_#{version.dots_to_underscores}_mac.pkg"
+  end
+
   name "Fury-800"
   desc "KORG Poly-800 simulation"
   homepage "https://www.fullbucket.de/music/fury800.html"
@@ -13,10 +19,20 @@ cask "fury800" do
     regex(/v(\d+(?:\.\d+)*)/)
   end
 
-  pkg "fury800_#{version.dots_to_underscores}_mac.pkg"
+  if MacOS.version <= :mojave
+    pkg "fury800_#{version.dots_to_underscores}_N_mac.pkg"
 
-  uninstall pkgutil: [
-    "de.fullbucket.audiounit.pkg.Fury800",
-    "de.fullbucket.vst.pkg.Fury800",
-  ]
+    uninstall pkgutil: [
+      "com.fullbucket.audiounit.pkg.Fury800",
+      "com.fullbucket.vst.pkg.Fury800",
+    ]
+  else
+    pkg "fury800_#{version.dots_to_underscores}_mac.pkg"
+
+    uninstall pkgutil: [
+      "de.fullbucket.audiounit.pkg.Fury800",
+      "de.fullbucket.vst.pkg.Fury800",
+      "de.fullbucket.vst3.pkg.Fury800",
+    ]
+  end
 end
