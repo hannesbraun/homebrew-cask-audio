53537d05ab0b391600f534c230b643a9ba56d514
Hannes Braun <hannesbraun@mail.de>
2022-04-12 20:21:28 +0200

    Update stigma from 1.2.7 to 1.3.0

--- Casks/stigma.rb
+++ Casks/stigma.rb
@@ -1,14 +1,8 @@
 cask "stigma" do
-  version "1.2.7"
+  version "1.3.0"
+  sha256 "ffd86b5c96355de141ae0542ec8fbe86b2a5fbcbcd6c9ac80caa1bc330c1fbb6"
 
-  if MacOS.version <= :mojave
-    sha256 "1ce37451515ef553885910d6e5dbd8c926abfab201352e781d33718f51d1008d"
-    url "https://www.fullbucket.de/music/dl/stigma_#{version.dots_to_underscores}_mac.pkg"
-  else
-    sha256 "f887fbea173e2af7268c90b7efe7244e454b3ad26d970b1196391e2e7da764a3"
-    url "https://www.fullbucket.de/music/dl/stigma_#{version.dots_to_underscores}_METAL_mac.pkg"
-  end
-
+  url "https://www.fullbucket.de/music/dl/stigma_#{version.dots_to_underscores}_mac.pkg"
   name "Stigma"
   desc "KORG Sigma KP-30 simulation"
   homepage "https://www.fullbucket.de/music/stigma.html"
@@ -19,14 +13,11 @@ cask "stigma" do
     regex(/v(\d+(?:\.\d+)*)/)
   end
 
-  if MacOS.version <= :mojave
-    pkg "stigma_#{version.dots_to_underscores}_mac.pkg"
-  else
-    pkg "stigma_#{version.dots_to_underscores}_METAL_mac.pkg"
-  end
+  pkg "stigma_#{version.dots_to_underscores}_mac.pkg"
 
   uninstall pkgutil: [
     "de.fullbucket.audiounit.pkg.Stigma",
     "de.fullbucket.vst.pkg.Stigma",
+    "de.fullbucket.vst3.pkg.Stigma",
   ]
 end
