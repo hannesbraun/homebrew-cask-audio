e547c846b7f4fb7f68b88b59ee516d9de3857881
Hannes Braun <hannesbraun@mail.de>
2022-04-12 12:59:23 +0200

    Update fb3100 from 1.1.7 to 1.2.0

--- Casks/fb3100.rb
+++ Casks/fb3100.rb
@@ -1,6 +1,6 @@
 cask "fb3100" do
-  version "1.1.7"
-  sha256 "7a224a6fba5215999348d1698c9a637b52dbb7856da66d125b60ffae0589c3f2"
+  version "1.2.0"
+  sha256 "86d74c946620a71665e67a7413eb7d7eee177336258544910f6702c4971ce86b"
 
   url "https://www.fullbucket.de/music/dl/fb3100_#{version.dots_to_underscores}_mac.pkg"
   name "FB-3100"
@@ -16,7 +16,8 @@ cask "fb3100" do
   pkg "fb3100_#{version.dots_to_underscores}_mac.pkg"
 
   uninstall pkgutil: [
-    "com.fullbucket.audiounit.pkg.FB3100",
-    "com.fullbucket.vst.pkg.FB3100",
+    "de.fullbucket.audiounit.pkg.FB3100",
+    "de.fullbucket.vst.pkg.FB3100",
+    "de.fullbucket.vst3.pkg.FB3100",
   ]
 end
