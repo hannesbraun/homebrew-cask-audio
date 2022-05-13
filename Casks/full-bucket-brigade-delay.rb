d0e6dccd54c904cd42ddd5ec8bc1c5dcf40c54bc
Hannes Braun <hannesbraun@mail.de>
2022-05-13 22:55:07 +0200

    Update full-bucket-brigade-delay from 1.2.4 to 2.0.0

--- Casks/full-bucket-brigade-delay.rb
+++ Casks/full-bucket-brigade-delay.rb
@@ -1,6 +1,6 @@
 cask "full-bucket-brigade-delay" do
-  version "1.2.4"
-  sha256 "f27c81043adc3b1a5500a4bfccd6f639fde7b35a8eeb0f3e9cd2d4d42d9bb9b6"
+  version "2.0.0"
+  sha256 "fa7285cb551a2ebaa505f342a1bb6654d3cbe3221ddb8845f3ffaef578bf56c7"
 
   url "https://www.fullbucket.de/music/dl/fbdelay_#{version.dots_to_underscores}_mac.pkg"
   name "Full Bucket Brigade Delay"
@@ -16,7 +16,8 @@ cask "full-bucket-brigade-delay" do
   pkg "fbdelay_#{version.dots_to_underscores}_mac.pkg"
 
   uninstall pkgutil: [
-    "com.fullbucket.audiounit.pkg.FBDelay",
-    "com.fullbucket.vst.pkg.FBDelay",
+    "de.fullbucket.audiounit.pkg.FBDelay",
+    "de.fullbucket.vst.pkg.FBDelay",
+    "de.fullbucket.vst3.pkg.FBDelay",
   ]
 end
