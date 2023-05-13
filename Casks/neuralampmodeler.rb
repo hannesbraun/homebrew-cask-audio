b17205cec22c14367435f4c77361746813f3b295
Juan Ignacio Donoso <jidonoso@gmail.com>
2023-05-13 13:49:39 -0400

    Add neuralampmodeler 0.7.3
    Co-authored-by: Hannes Braun <hannes@hannesbraun.net>

---
+++ Casks/neuralampmodeler.rb
@@ -0,0 +1,20 @@
+cask "neuralampmodeler" do
+  version "0.7.3"
+  sha256 "05a2caf404e4e100e97e538b7b1bd337fd7f7f74b7f00d5e801a312dfc257d3f"
+
+  url "https://github.com/sdatkinson/NeuralAmpModelerPlugin/releases/download/v#{version}/NeuralAmpModeler-v#{version}-mac.dmg"
+  name "Neural Amp Modeler"
+  desc "VST3/AudioUnit plug-in* for Neural Amp Modeler, built with iPlug2"
+  homepage "https://github.com/sdatkinson/NeuralAmpModelerPlugin"
+
+  livecheck do
+    strategy :git
+    url "https://github.com/sdatkinson/NeuralAmpModelerPlugin/"
+  end
+
+  pkg "NeuralAmpModeler Installer.pkg"
+
+  uninstall pkgutil: [
+    "com.StevenAtkinson.app.NeuralAmpModeler",
+  ]
+end
