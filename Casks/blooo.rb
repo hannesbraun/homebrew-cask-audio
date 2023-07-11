50be0b0dfa504be2cd50ea708cd7bb3028d571b2
Hannes Braun <hannes@hannesbraun.net>
2023-07-11 18:06:45 +0200

    blooo 2.3.2

--- Casks/blooo.rb
+++ Casks/blooo.rb
@@ -1,6 +1,6 @@
 cask "blooo" do
-  version "2.3.1"
-  sha256 "b39a8c9ca7a8b743ddf2e7b86d508f2977da821ec48a76451885141f6b2c7a29"
+  version "2.3.2"
+  sha256 "08da656d8c74525a760decba20e1a6f32eb9b0732f1d9a822d8089d7811cb480"
 
   url "https://www.fullbucket.de/music/dl/blooo_#{version.dots_to_underscores}_mac.pkg"
   name "the blooo"
