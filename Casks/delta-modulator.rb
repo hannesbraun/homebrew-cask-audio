84acb21e985486f49e4ead4d30bf4b3d3633c3ff
Hannes Braun <hannes@hannesbraun.net>
2023-08-11 18:44:06 +0200

    delta-modulator: update to latest version

--- Casks/delta-modulator.rb
+++ Casks/delta-modulator.rb
@@ -1,12 +1,11 @@
 cask "delta-modulator" do
-  version "1.06"
-  sha256 "0608ebc52f7d3b0fa5e8d59717e17c081b0f8bce05dee78af251aac793deeadd"
+  version :latest
+  sha256 :no_check
 
   url do
     require "open-uri"
     URI("https://xferrecords.com/product_downloads/26/freeware").open.base_uri.to_s
   end
-  appcast "https://xferrecords.com/freeware/"
   name "Delta Modulator"
   homepage "https://xferrecords.com/freeware/"
 
