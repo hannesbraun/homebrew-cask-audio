1de7829256678f936f631b34b816b18f371d3656
Hannes Braun <hannes@hannesbraun.net>
2023-08-13 00:16:29 +0200

    fb7999 1.1.3

--- Casks/fb7999.rb
+++ Casks/fb7999.rb
@@ -1,32 +1,8 @@
 cask "fb7999" do
-  version "1.1.1"
+  version "1.1.3"
+  sha256 "4e83f20a5ba748428623f41baad6e2415fefedb3115aefb599eae80e31411859"
 
-  on_mojave :or_older do
-    sha256 "6955338793d12b9b3da368bd442279e4817f59e7bac1ad908b6e20833fa2ec1b"
-
-    url "https://www.fullbucket.de/music/dl/fb7999_#{version.dots_to_underscores}_N_mac.pkg"
-
-    pkg "fb7999_#{version.dots_to_underscores}_N_mac.pkg"
-
-    uninstall pkgutil: [
-      "com.fullbucket.audiounit.pkg.FB7999",
-      "com.fullbucket.vst.pkg.FB7999",
-    ]
-  end
-  on_catalina :or_newer do
-    sha256 "30870ab76c5d69c6aa049ff51238509118ffc5d6453a8faec19473bd1c0771ee"
-
-    url "https://www.fullbucket.de/music/dl/fb7999_#{version.dots_to_underscores}_mac.pkg"
-
-    pkg "fb7999_#{version.dots_to_underscores}_mac.pkg"
-
-    uninstall pkgutil: [
-      "de.fullbucket.audiounit.pkg.FB7999",
-      "de.fullbucket.vst.pkg.FB7999",
-      "de.fullbucket.vst3.pkg.FB7999",
-    ]
-  end
-
+  url "https://www.fullbucket.de/music/dl/fb7999_#{version.dots_to_underscores}_mac.pkg"
   name "FB-7999"
   desc "Digital waveform synthesizer simulation"
   homepage "https://www.fullbucket.de/music/fb7999.html"
@@ -36,4 +12,12 @@ cask "fb7999" do
     strategy :page_match
     regex(/v(\d+(?:\.\d+)*)/)
   end
+
+  pkg "fb7999_#{version.dots_to_underscores}_mac.pkg"
+
+  uninstall pkgutil: [
+    "de.fullbucket.audiounit.pkg.FB7999",
+    "de.fullbucket.vst.pkg.FB7999",
+    "de.fullbucket.vst3.pkg.FB7999",
+  ]
 end
