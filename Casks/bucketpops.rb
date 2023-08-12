b26f7850e9052bd86c6c5567497e0d4482b03105
Hannes Braun <hannes@hannesbraun.net>
2023-08-13 00:15:30 +0200

    bucketpops 1.1.5

--- Casks/bucketpops.rb
+++ Casks/bucketpops.rb
@@ -1,32 +1,8 @@
 cask "bucketpops" do
-  version "1.1.1"
+  version "1.1.5"
+  sha256 "458f5086d72dc8b995f85399616b7d3a4118da2358567c5ee1eaed5814422f26"
 
-  on_mojave :or_older do
-    sha256 "35215957aeff6cacf1ed81b742ac5450f894b6bfa16c753cf0b4124c28689c74"
-
-    url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_N_mac.pkg"
-
-    pkg "bucketpops_#{version.dots_to_underscores}_N_mac.pkg"
-
-    uninstall pkgutil: [
-      "com.fullbucket.audiounit.pkg.BucketPops",
-      "com.fullbucket.vst.pkg.BucketPops",
-    ]
-  end
-  on_catalina :or_newer do
-    sha256 "d00e0e04641531e5b5a04dbd1080db54ff5e642f2c396980345b50231503d85f"
-
-    url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_mac.pkg"
-
-    pkg "bucketpops_#{version.dots_to_underscores}_mac.pkg"
-
-    uninstall pkgutil: [
-      "de.fullbucket.audiounit.pkg.BucketPops",
-      "de.fullbucket.vst.pkg.BucketPops",
-      "de.fullbucket.vst3.pkg.BucketPops",
-    ]
-  end
-
+  url "https://www.fullbucket.de/music/dl/bucketpops_#{version.dots_to_underscores}_mac.pkg"
   name "Bucket Pops"
   desc "KORG Mini Pops-7 simulation"
   homepage "https://www.fullbucket.de/music/bucketpops.html"
@@ -36,4 +12,12 @@ cask "bucketpops" do
     strategy :page_match
     regex(/v(\d+(?:\.\d+)*)/)
   end
+
+  pkg "bucketpops_#{version.dots_to_underscores}_mac.pkg"
+
+  uninstall pkgutil: [
+    "de.fullbucket.audiounit.pkg.BucketPops",
+    "de.fullbucket.vst.pkg.BucketPops",
+    "de.fullbucket.vst3.pkg.BucketPops",
+  ]
 end
