a0f069a3bdc899686fb01bcacf6ab41a8f69225d
Hannes Braun <hannes@hannesbraun.net>
2023-08-12 23:48:51 +0200

    fb3300 1.2.2

--- Casks/fb3300.rb
+++ Casks/fb3300.rb
@@ -1,32 +1,8 @@
 cask "fb3300" do
-  on_mojave :or_older do
-    version "1.2.0"
-    sha256 "0e1fe376647ab1aebf7d961843027f3e84c76b736263210eb4e720990f21a305"
+  version "1.2.2"
+  sha256 "6fe9c7fcc6735a7dfc0e4231a34ab05a06ca61f99ebd39008b636b5356a768aa"
 
-    url "https://www.fullbucket.de/music/dl/fb3300_#{version.dots_to_underscores}_N_mac.pkg"
-
-    pkg "fb3300_#{version.dots_to_underscores}_N_mac.pkg"
-
-    uninstall pkgutil: [
-      "com.fullbucket.audiounit.pkg.FB3300",
-      "com.fullbucket.vst.pkg.FB3300",
-    ]
-  end
-  on_catalina :or_newer do
-    version "1.2.1"
-    sha256 "75ae81d27f1671ac5b23624573c2c0006d0c0d47bb61784bc26705c543c9fbfb"
-
-    url "https://www.fullbucket.de/music/dl/fb3300_#{version.dots_to_underscores}_mac.pkg"
-
-    pkg "fb3300_#{version.dots_to_underscores}_mac.pkg"
-
-    uninstall pkgutil: [
-      "de.fullbucket.audiounit.pkg.FB3300",
-      "de.fullbucket.vst.pkg.FB3300",
-      "de.fullbucket.vst3.pkg.FB3300",
-    ]
-  end
-
+  url "https://www.fullbucket.de/music/dl/fb3300_#{version.dots_to_underscores}_mac.pkg"
   name "FB-3300"
   desc "KORG PS-3300 simulation"
   homepage "https://www.fullbucket.de/music/fb3300.html"
@@ -36,4 +12,12 @@ cask "fb3300" do
     strategy :page_match
     regex(/v(\d+(?:\.\d+)*)/)
   end
+
+  pkg "fb3300_#{version.dots_to_underscores}_mac.pkg"
+
+  uninstall pkgutil: [
+    "de.fullbucket.audiounit.pkg.FB3300",
+    "de.fullbucket.vst.pkg.FB3300",
+    "de.fullbucket.vst3.pkg.FB3300",
+  ]
 end
