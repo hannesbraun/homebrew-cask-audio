97ca33e52c8a81616a8e5c9959c87d24a950537e
Hannes Braun <hannes@hannesbraun.net>
2023-08-13 00:11:46 +0200

    fb3200 1.1.1

--- Casks/fb3200.rb
+++ Casks/fb3200.rb
@@ -1,6 +1,6 @@
 cask "fb3200" do
-  version "1.1.0"
-  sha256 "24d2295b5364af4dc727447c1754fba9f90b7beea3307774e2063542f5eef5fa"
+  version "1.1.1"
+  sha256 "6febad45bcaf33c834e70030646297861ec91e959dc147dfeafc101cf1eacf6b"
 
   url "https://www.fullbucket.de/music/dl/fb3200_#{version.dots_to_underscores}_mac.pkg"
   name "FB-3200"
