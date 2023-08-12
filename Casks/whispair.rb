f7b7dc496a8ea3176ce94826eb3d5e8ea75b3a2d
Hannes Braun <hannes@hannesbraun.net>
2023-08-13 00:06:52 +0200

    whispair 1.1.4

--- Casks/whispair.rb
+++ Casks/whispair.rb
@@ -1,32 +1,8 @@
 cask "whispair" do
-  version "1.1.1"
+  version "1.1.4"
+  sha256 "529209aeaa17a449b9cf4a3d8306db000f4cbb43fa531ba4328711636d4dfbdf"
 
-  on_mojave :or_older do
-    sha256 "86ee4f3b6423d30284f318a944efdce9aad493eebea442abe778f4902ae2147f"
-
-    url "https://www.fullbucket.de/music/dl/whispair_#{version.dots_to_underscores}_N_mac.pkg"
-
-    pkg "whispair_#{version.dots_to_underscores}_N_mac.pkg"
-
-    uninstall pkgutil: [
-      "com.fullbucket.audiounit.pkg.WhispAir",
-      "com.fullbucket.vst.pkg.WhispAir",
-    ]
-  end
-  on_catalina :or_newer do
-    sha256 "1566de2749703d1fd12feddfb3d73e70017b309e944dddcd590bb6ecc961b454"
-
-    url "https://www.fullbucket.de/music/dl/whispair_#{version.dots_to_underscores}_mac.pkg"
-
-    pkg "whispair_#{version.dots_to_underscores}_mac.pkg"
-
-    uninstall pkgutil: [
-      "de.fullbucket.audiounit.pkg.WhispAir",
-      "de.fullbucket.vst.pkg.WhispAir",
-      "de.fullbucket.vst3.pkg.WhispAir",
-    ]
-  end
-
+  url "https://www.fullbucket.de/music/dl/whispair_#{version.dots_to_underscores}_mac.pkg"
   name "WhispAir"
   desc "Wavetable synthesizer"
   homepage "https://www.fullbucket.de/music/whispair.html"
@@ -36,4 +12,12 @@ cask "whispair" do
     strategy :page_match
     regex(/v(\d+(?:\.\d+)*)/)
   end
+
+  pkg "whispair_#{version.dots_to_underscores}_mac.pkg"
+
+  uninstall pkgutil: [
+    "de.fullbucket.audiounit.pkg.WhispAir",
+    "de.fullbucket.vst.pkg.WhispAir",
+    "de.fullbucket.vst3.pkg.WhispAir",
+  ]
 end
