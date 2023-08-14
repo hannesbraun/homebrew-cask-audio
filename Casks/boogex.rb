d427c082db74adcc5c987bd083785e9cc5dd45e3
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 12:59:00 +0200

    boogex 3.7

--- Casks/boogex.rb
+++ Casks/boogex.rb
@@ -1,13 +1,20 @@
 cask "boogex" do
-  version "3.3"
-  sha256 "e74c1d852a64f8dd4cfcf5e04900505f6f5f4010ae4dce562717d74c60392e51"
+  version "3.7"
+  sha256 "ea06e4da75f57dbae8c712f2f945b19850312c8ff2ab58c7fb43a16229aab169"
 
   url "https://www.voxengo.com/files/VoxengoBoogex_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  appcast "https://www.voxengo.com/product/boogex/",
-          must_contain: version.no_dots
   name "Voxengo Boogex"
   desc "Guitar amp plugin"
   homepage "https://www.voxengo.com/product/boogex/"
+
+  livecheck do
+    url "https://www.voxengo.com/product/boogex/"
+    strategy :page_match do |page|
+      page.scan(/VoxengoBoogex_(\d)(\d+)_Mac_AU_AAX_setup\.dmg/).map do |match|
+        "#{match[0]}.#{match[1]}"
+      end
+    end
+  end
 
   audio_unit_plugin "Boogex.component"
   artifact "ProTools AAX/Boogex.aaxplugin",
