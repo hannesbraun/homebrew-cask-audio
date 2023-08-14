716ec18e553633225afdd0d08a7b2134bce5c32c
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 20:00:52 +0200

    full-bucket-brigade-delay: fix sha256

--- Casks/full-bucket-brigade-delay.rb
+++ Casks/full-bucket-brigade-delay.rb
@@ -1,6 +1,6 @@
 cask "full-bucket-brigade-delay" do
   version "2.0.0"
-  sha256 "7cbe9dc67b75149c4f14c4a8b6c82ffb599dc6e7a92802e6f69fd5c99f659efd"
+  sha256 "bca0d3c4537f91cf1ead4e667b6cae6996e2bb1ed2108870d7ece8da5adca408"
 
   url "https://www.fullbucket.de/music/dl/fbdelay_#{version.dots_to_underscores}_mac.pkg"
   name "Full Bucket Brigade Delay"
