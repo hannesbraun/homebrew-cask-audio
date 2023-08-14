3668705136aa00608997216e639a1925cf8e9d6e
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 12:59:11 +0200

    span 3.19

--- Casks/span.rb
+++ Casks/span.rb
@@ -1,13 +1,20 @@
 cask "span" do
-  version "3.15"
-  sha256 "dfacb8f8567d5e2b7b971ef189c9f4a9723eb9452e45c4a6810e2d50a29ab615"
+  version "3.19"
+  sha256 "485b1c44bcca3878790d032de09dbb6c84171885155bcbec76d1ffce5f159cff"
 
   url "https://www.voxengo.com/files/VoxengoSPAN_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  appcast "https://www.voxengo.com/product/span/",
-          must_contain: version.no_dots
   name "Voxengo SPAN"
   desc "Spectrum analyzer plugin"
   homepage "https://www.voxengo.com/product/span/"
+
+  livecheck do
+    url "https://www.voxengo.com/product/span/"
+    strategy :page_match do |page|
+      page.scan(/VoxengoSPAN_(\d)(\d+)_Mac_AU_AAX_setup\.dmg/).map do |match|
+        "#{match[0]}.#{match[1]}"
+      end
+    end
+  end
 
   audio_unit_plugin "SPAN.component"
   artifact "ProTools AAX/SPAN.aaxplugin",
