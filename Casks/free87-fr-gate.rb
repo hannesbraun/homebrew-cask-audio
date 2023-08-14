61e607b9c8521388ba2a720a563eaee7a9c55c1a
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:19:10 +0200

    free87-fr-gate: add livecheck

--- Casks/free87-fr-gate.rb
+++ Casks/free87-fr-gate.rb
@@ -3,10 +3,17 @@ cask "free87-fr-gate" do
   sha256 "16d316679cc131d1fd591370f9acec2c4a6414779dacfe4494fd92e2bcdd64a6"
 
   url "https://www.eareckon.com/free/FRGATE87_#{version.no_dots}_Mac.zip"
-  appcast "https://www.eareckon.com/en/products/freebies.html",
-          must_contain: version.no_dots
   name "FR-GATE 87"
   homepage "https://www.eareckon.com/en/products/freebies.html"
+
+  livecheck do
+    url "https://www.eareckon.com/en/products/freebies.html"
+    strategy :page_match do |page|
+      page.scan(/FRGATE87_(\d)(\d)(\d)_Mac\.zip/).map do |match|
+        "#{match[0]}.#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   pkg "Setup_FRGATE87_#{version.no_dots}.pkg"
 
