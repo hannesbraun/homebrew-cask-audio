bc810ac90420a2643460ab594684af3536b20315
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 20:13:46 +0200

    ob-xd 2.14

--- Casks/ob-xd.rb
+++ Casks/ob-xd.rb
@@ -1,16 +1,16 @@
 cask "ob-xd" do
-  version "2.10"
-  sha256 "17727448f7a68e92bae4811c87aa381bf6cea75617b2506f77f60406ef2cedfb"
+  version "2.14"
+  sha256 "20540cdd9b6cbe841c4bc0962e03a1c45c7d343db1963db86b35f0b4aa0f43f2"
 
-  url "https://www.discodsp.net/download/Obxd#{version.major_minor.no_dots}Mac.zip",
-      verified: "discodsp.net/"
+  url "https://demo.discodsp.com/Obxd#{version.major_minor.no_dots}Mac.zip"
   name "OB-Xd"
   desc "Virtual analog synthesizer"
   homepage "https://www.discodsp.com/obxd/"
 
   livecheck do
-    strategy :git
-    url "https://github.com/reales/OB-Xd/"
+    strategy :page_match
+    url :homepage
+    regex(/OB-Xd.*(\d+(?:\.\d+)+)/)
   end
 
   pkg "OB-Xd #{version.major_minor}.pkg"
