935fa15e16a2ac583b0bbaa3f157acf7310a54fc
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:18:07 +0200

    oldskoolverb 2.12

--- Casks/oldskoolverb.rb
+++ Casks/oldskoolverb.rb
@@ -1,13 +1,20 @@
 cask "oldskoolverb" do
-  version "2.7"
-  sha256 "6a0a2038848077997f359ed86481426cf4f6cc10ff2d644f13ed439269af4ec1"
+  version "2.12"
+  sha256 "39cec78d4771e2b94e32cd829d8baff15fbad34e227a0287d337e80925f2aca4"
 
   url "https://www.voxengo.com/files/VoxengoOldSkoolVerb_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  appcast "https://www.voxengo.com/product/oldskoolverb/",
-          must_contain: version.no_dots
   name "Voxengo OldSkoolVerb"
   desc "Plate reverb plugin"
   homepage "https://www.voxengo.com/product/oldskoolverb/"
+
+  livecheck do
+    url "https://www.voxengo.com/product/oldskoolverb/"
+    strategy :page_match do |page|
+      page.scan(/VoxengoOldSkoolVerb_(\d)(\d+)_Mac_AU_AAX_setup\.dmg/).map do |match|
+        "#{match[0]}.#{match[1]}"
+      end
+    end
+  end
 
   audio_unit_plugin "OldSkoolVerb.component"
   artifact "ProTools AAX/OldSkoolVerb.aaxplugin",
