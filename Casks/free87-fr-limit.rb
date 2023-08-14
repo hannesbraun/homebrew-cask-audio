08b736a2352777bd8f4c74dd39e6783378029df1
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:19:19 +0200

    free87-fr-limit: add livecheck

--- Casks/free87-fr-limit.rb
+++ Casks/free87-fr-limit.rb
@@ -3,10 +3,17 @@ cask "free87-fr-limit" do
   sha256 "fc054f2ae8cc279a87f85df25ebdb2050fe60a3035e7c47239166e6fe4b8db78"
 
   url "https://www.eareckon.com/free/FRLIMIT87_#{version.no_dots}_Mac.zip"
-  appcast "https://www.eareckon.com/en/products/freebies.html",
-          must_contain: version.no_dots
   name "FR-LIMIT 87"
   homepage "https://www.eareckon.com/en/products/freebies.html"
+
+  livecheck do
+    url "https://www.eareckon.com/en/products/freebies.html"
+    strategy :page_match do |page|
+      page.scan(/FRLIMIT87_(\d)(\d)(\d)_Mac\.zip/).map do |match|
+        "#{match[0]}.#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   pkg "Setup_FRLIMIT87_#{version.no_dots}.pkg"
 
