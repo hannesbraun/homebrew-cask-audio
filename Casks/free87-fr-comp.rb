163877002fc95a08a59d2b4f115cfada4dcaa4fc
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:18:42 +0200

    free87-fr-comp: add livecheck

--- Casks/free87-fr-comp.rb
+++ Casks/free87-fr-comp.rb
@@ -3,10 +3,17 @@ cask "free87-fr-comp" do
   sha256 "3156ed51a5bd6b515d71cbc5ea9eecec26c481c38e8f8041a0cc0fdcb2e43639"
 
   url "https://www.eareckon.com/free/FRCOMP87_#{version.no_dots}_Mac.zip"
-  appcast "https://www.eareckon.com/en/products/freebies.html",
-          must_contain: version.no_dots
   name "FR-COMP 87"
   homepage "https://www.eareckon.com/en/products/freebies.html"
+
+  livecheck do
+    url "https://www.eareckon.com/en/products/freebies.html"
+    strategy :page_match do |page|
+      page.scan(/FRCOMP87_(\d)(\d)(\d)_Mac\.zip/).map do |match|
+        "#{match[0]}.#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   pkg "Setup_FRCOMP87_#{version.no_dots}.pkg"
 
