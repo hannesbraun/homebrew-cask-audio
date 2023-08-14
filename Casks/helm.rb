161baf42f8139b1a82662ab0a3eef3b6b25d79d6
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:21:47 +0200

    helm: add livecheck

--- Casks/helm.rb
+++ Casks/helm.rb
@@ -3,10 +3,14 @@ cask "helm" do
   sha256 "1bfac2c016d7dd5cd51e714fa6d9facef4e1db52617ff7dd52748dbec1400ebf"
 
   url "https://tytel.org/static/dist/Helm_v#{version.dots_to_underscores}_r.pkg"
-  appcast "https://github.com/mtytel/helm/releases.atom"
   name "Helm"
   desc "Free and open source polyphonic synthesizer"
   homepage "https://tytel.org/helm/"
+
+  livecheck do
+    strategy :git
+    url "https://github.com/mtytel/helm/"
+  end
 
   pkg "Helm_v#{version.dots_to_underscores}_r.pkg"
 
