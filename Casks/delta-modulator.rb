2179b6192233fe8586f24ff917bd910b8c02d0c3
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 19:56:44 +0200

    delta-modulator: add description and verified parameter

--- Casks/delta-modulator.rb
+++ Casks/delta-modulator.rb
@@ -4,9 +4,11 @@ cask "delta-modulator" do
 
   url do
     require "open-uri"
-    URI("https://xferrecords.com/product_downloads/26/freeware").open.base_uri.to_s
+    [URI("https://xferrecords.com/product_downloads/26/freeware").open.base_uri.to_s,
+     { verified: "xfer-active-storage.s3.us-west-1.amazonaws.com" }]
   end
   name "Delta Modulator"
+  desc "Bitcrusher plugin"
   homepage "https://xferrecords.com/freeware/"
 
   pkg "Install_Xfer_DeltaModulator.pkg"
