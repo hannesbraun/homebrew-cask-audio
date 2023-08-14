3d6f36c0cd98a3d82240cb3650b1c1aa990588f6
Hannes Braun <hannes@hannesbraun.net>
2023-08-14 13:19:00 +0200

    free87-fr-equa: add livecheck

--- Casks/free87-fr-equa.rb
+++ Casks/free87-fr-equa.rb
@@ -3,10 +3,17 @@ cask "free87-fr-equa" do
   sha256 "7da16e94df6fac759ac03281c64adb85ddfe26be2de4ba77a606c311fd0d673f"
 
   url "https://www.eareckon.com/free/FREQUA87_#{version.no_dots}_Mac.zip"
-  appcast "https://www.eareckon.com/en/products/freebies.html",
-          must_contain: version.no_dots
   name "FR-EQUA 87"
   homepage "https://www.eareckon.com/en/products/freebies.html"
+
+  livecheck do
+    url "https://www.eareckon.com/en/products/freebies.html"
+    strategy :page_match do |page|
+      page.scan(/FREQUA87_(\d)(\d)(\d)_Mac\.zip/).map do |match|
+        "#{match[0]}.#{match[1]}.#{match[2]}"
+      end
+    end
+  end
 
   pkg "Setup_FREQUA87_#{version.no_dots}.pkg"
 
