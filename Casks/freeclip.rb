fe55a56525c0125a9b53e87812787320b1b37cd9
Hannes Braun <hannes@hannesbraun.net>
2023-08-15 21:28:39 +0200

    freeclip: fix url and sha256

--- Casks/freeclip.rb
+++ Casks/freeclip.rb
@@ -1,9 +1,8 @@
 cask "freeclip" do
   version "0.9.2"
-  sha256 "1e3a2356acd3249a3054e00c6be22cf364e4863ba71931b5440bc9f93878e38e"
+  sha256 "bfa4366e7b5c69449eae17aeeb2b33d81cef4d7158d45aa7a49b30d80aa78586"
 
-  url "https://www.vennaudio.com/downloads/Free%20Clip%20#{version}.dmg"
-  appcast "https://www.vennaudio.com/free-clip/"
+  url "https://www.vennaudio.com/downloads/FreeClip/Free%20Clip%20#{version}.dmg"
   name "Free Clip"
   desc "Intuitive multi algorithm soft clipper"
   homepage "https://www.vennaudio.com/free-clip/"
