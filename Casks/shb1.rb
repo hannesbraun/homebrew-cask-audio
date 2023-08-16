fa6962dbfdb3f490819b138aec9565066d2708a0
Hannes Braun <hannes@hannesbraun.net>
2023-08-17 00:45:54 +0200

    shb1: add description, update homepage and remove comment

--- Casks/shb1.rb
+++ Casks/shb1.rb
@@ -2,11 +2,11 @@ cask "shb1" do
   version "1.0.0"
   sha256 "3c9dfd95a585cac1f1dc83a1db6132428e46d294641dc2129fb2d6d77cbf5479"
 
-  # static.kvraudio.com was verified as official when first introduced to the cask
   url "https://static.kvraudio.com/files/1546/ignite_amps_shb-1_#{version.dots_to_underscores}_mac.zip",
       verified: "static.kvraudio.com/"
   name "SHB-1"
-  homepage "http://www.igniteamps.com/"
+  desc "Amp based on the real SHB-1 \"Extreme Bass Head\""
+  homepage "https://www.igniteamps.com/"
 
   livecheck do
     url :homepage
