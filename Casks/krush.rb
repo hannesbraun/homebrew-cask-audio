2260a993af1e4ced804484b934dc2e71d64ff36a
Hannes Braun <hannes@hannesbraun.net>
2023-08-16 22:05:18 +0200

    krush 1.2.5

--- Casks/krush.rb
+++ Casks/krush.rb
@@ -1,12 +1,17 @@
 cask "krush" do
-  version "1.1.4"
-  sha256 "da2fb82493600c1431f3903ce578c54cdf36bf0bfb2bfe7b7c117213e2cc9697"
+  version "1.2.5"
+  sha256 "5f3f03d07b7d13f5fd2b1cdb5b48137d1e43d4000a89b0f34403fdbe5687c642"
 
-  url "https://www.tritik.com/downloads/plugins/krush/Tritik_Krush_v#{version}.dmg"
-  appcast "https://www.tritik.com/releaseNotes/krush/changeLog.php"
+  url "https://www.tritik.com/downloads/plugins/krush/Tritik_Krush_v#{version}_macOS.zip"
   name "Krush"
   desc "Bitcrusher"
   homepage "https://www.tritik.com/products/krush/"
+
+  livecheck do
+    strategy :page_match
+    url :homepage
+    regex(/version (\d+(?:\.\d+)*)/)
+  end
 
   pkg "Tritik_Krush_v#{version}.pkg"
 
@@ -16,5 +21,6 @@ cask "krush" do
     "com.tritik.pkg.Tritik-Krush-AAX",
     "com.tritik.pkg.Tritik-Krush-AU",
     "com.tritik.pkg.Tritik-Krush-VST",
+    "com.tritik.pkg.Tritik-Krush-VST3",
   ]
 end
