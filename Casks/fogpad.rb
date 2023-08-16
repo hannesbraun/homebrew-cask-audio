9e7e72c0350a88e1fc6dadae8646279f8b562fab
Hannes Braun <hannes@hannesbraun.net>
2023-08-17 00:30:39 +0200

    regrader: add livecheck and fix download

--- Casks/fogpad.rb
+++ Casks/fogpad.rb
@@ -1,11 +1,20 @@
 cask "fogpad" do
-  version "1.0.2,83b12af28e733df71aaa5621e384d572"
-  sha256 "3d25036b40acb37244994ab727c6a5e5fadee3adfee34ee36f6dfcef7a1b81ca"
+  version "1.0.2,777c289f331ee762da84b0a889b83d8b4fd9729d78c3e2041c817cb4061fed8e"
+  sha256 "a7675fa3ef13f14267a7c2284b8245cdad9f97a8b077621ab354601d41c00516"
 
   url "https://www.igorski.nl/download/retrieve/#{version.csv.second}"
-  appcast "https://www.igorski.nl/download/fogpad--reverb-plugin"
   name "FogPad"
-  homepage "https://www.igorski.nl/download/fogpad--reverb-plugin"
+  desc "Reverb plugin"
+  homepage "https://www.igorski.nl/download/fogpad"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{retrieve/(\w+)"(?:.|\n)*title="Download file fogpad\.(\d+(?:\.\d+)*)\.zip}).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   audio_unit_plugin "macOS/AU/fogpad.component"
   vst_plugin "macOS/VST/fogpad.vst"
