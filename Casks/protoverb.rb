8cef565038fd68d87cefda116b123796d4cb8140
Hannes Braun <hannes@hannesbraun.net>
2023-08-17 01:30:47 +0200

    protoverb 1.0.1

--- Casks/protoverb.rb
+++ Casks/protoverb.rb
@@ -1,20 +1,31 @@
 cask "protoverb" do
-  version "1.0,4102"
-  sha256 "13db2f87ec9084a5f2b03ea3a6280dfd685993892d5cd404ceccce975a1dd920"
+  version "1.0.1,12092"
+  sha256 "ef229da0935eba9f525c60acbb579d0d395e2135624257cb89c39111c82a0f8d"
 
-  url "https://uhedownloads-heckmannaudiogmb.netdna-ssl.com/releases/Protoverb_#{version.csv.first.dots_to_underscores}_Mac.zip",
-      verified: "uhedownloads-heckmannaudiogmb.netdna-ssl.com/"
+  url "https://dl.u-he.com/releases/Protoverb_#{version.csv.first.no_dots}_#{version.csv.second}_Mac.zip"
   name "Protoverb"
+  desc "Experimental research reverb"
   homepage "https://u-he.com/products/protoverb/"
 
-  pkg "Protoverb#{version.after_comma}Mac/Protoverb #{version.before_comma}.#{version.after_comma} Installer.pkg"
+  livecheck do
+    url "https://u-he.com/products/protoverb/releasenotes.html"
+    strategy :page_match do |page|
+      match = page.match(/Protoverb\s*(\d+(?:\.\d+)*)\s*\(revision\s*(\d+(?:\.\d+)*)\)/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  pkg "Protoverb#{version.csv.second}Mac/Protoverb #{version.before_comma} Installer.pkg"
 
   uninstall pkgutil: [
-    "com.u-he.Protoverb.aax.pkg",
     "com.u-he.Protoverb.au.pkg",
     "com.u-he.Protoverb.data.pkg",
     "com.u-he.Protoverb.documentation.pkg",
     "com.u-he.Protoverb.presets.pkg",
+    "com.u-he.Protoverb.tuningFiles.pkg",
     "com.u-he.Protoverb.vst.pkg",
+    "com.u-he.Protoverb.vst3.pkg",
   ]
 end
