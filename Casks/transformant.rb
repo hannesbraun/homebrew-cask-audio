c89606a2265a43eee8b84926a8e10a886e6abfea
Hannes Braun <hannes@hannesbraun.net>
2023-08-17 01:03:45 +0200

    transformant 1.0.3

--- Casks/transformant.rb
+++ Casks/transformant.rb
@@ -1,11 +1,20 @@
 cask "transformant" do
-  version "1.0.2,13e0ea27fe9a4b557d48829a7f8bd001"
-  sha256 "4274bd55d077d043819b52d4d24ae821209740263cad1e2061fec8b11e9e0ebe"
+  version "1.0.3,5479b625bcdd53d5018a34c590947305ac83e3ea4ec5f61045edb64eeb64fc26"
+  sha256 "4df6f0414f7bbe7a0de689dff31dc37d264f9ff53d63f974ba53f0e0bcaecaf8"
 
   url "https://www.igorski.nl/download/retrieve/#{version.csv.second}"
-  appcast "https://www.igorski.nl/download/transformant--multichannel-formantic-noise-processor"
   name "Transformant"
-  homepage "https://www.igorski.nl/download/transformant--multichannel-formantic-noise-processor"
+  desc "Formantic noise processor"
+  homepage "https://www.igorski.nl/download/transformant"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{retrieve/(\w+)"(?:.|\n)*title="Download file transformant\.(\d+(?:\.\d+)*)\.zip}).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   audio_unit_plugin "macOS/Audio Unit/transformant_au.component"
   vst_plugin "macOS/VST/transformant.vst"
