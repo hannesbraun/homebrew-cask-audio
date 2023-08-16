b4dd6d021fe10d13d9ec9f7e6726c8024b151863
Hannes Braun <hannes@hannesbraun.net>
2023-08-16 22:47:26 +0200

    regrader: add livecheck and fix download

--- Casks/regrader.rb
+++ Casks/regrader.rb
@@ -1,12 +1,20 @@
 cask "regrader" do
-  version "1.0.4,b195b368ca4cda2be291c65bd177eab7"
-  sha256 "8be335234a06151d4ce797fa960d68c7435f52cd7c82e9e02e4b79192fa6a85b"
+  version "1.0.4,484444ad43fa2db726e293fb3001fff19d04336e9f5b0e959390577b9287a19c"
+  sha256 "00dd36b616b33ba826ffddb37534e255633669807081c6086e1adacc2da74452"
 
   url "https://www.igorski.nl/download/retrieve/#{version.csv.second}"
-  appcast "https://www.igorski.nl/download/regrader--degenerative-delay-processor"
   name "Regrader"
   desc "Degenerative delay processor"
-  homepage "https://www.igorski.nl/download/regrader--degenerative-delay-processor"
+  homepage "https://www.igorski.nl/download/regrader"
+
+  livecheck do
+    url :homepage
+    strategy :page_match do |page|
+      page.scan(%r{retrieve/(\w+)"(?:.|\n)*title="Download file regrader\.(\d+(?:\.\d+)*)\.zip}).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
 
   audio_unit_plugin "macOS/AU/regrader.component"
   vst_plugin "macOS/VST/regrader.vst"
