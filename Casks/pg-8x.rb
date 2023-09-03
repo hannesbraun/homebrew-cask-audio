85cccb2c1abb043caa5b1195abe3b14f71f6ec6c
Hannes Braun <hannes@hannesbraun.net>
2023-09-03 14:07:15 +0200

    pg-8x: add livecheck

--- Casks/pg-8x.rb
+++ Casks/pg-8x.rb
@@ -3,11 +3,15 @@ cask "pg-8x" do
   sha256 "68e49fe103ba69c390229cf2c8bd9353ab7d587bcc5ba036d32ebcf053032bef"
 
   url "https://drive.google.com/uc?export=download&id=1zbSGazrU2r0HRH_rskQk1kXRj10nyKFJ"
-  appcast "https://sites.google.com/site/mlvst0/pg8x-#{version.major}",
-          must_contain: version.major_minor
   name "PG-8X"
   desc "Virtual analog synthesizer"
   homepage "https://sites.google.com/site/mlvst0/pg8x-#{version.major}"
+
+  livecheck do
+    strategy :page_match
+    url "https://www.kvraudio.com/product/pg-8x-by-ml-vst"
+    regex(%r{<div .*id="verosx".*>(\d+(?:\.\d+)*)</div>})
+  end
 
   vst_plugin "PG-8X.vst"
 end
