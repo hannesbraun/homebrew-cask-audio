e142a639a8957493cdcc4f5886217d9322dff236
Hannes Braun <hannes@hannesbraun.net>
2024-10-04 14:07:55 +0200

    tal-noisemaker 5.0.6

--- Casks/tal-noisemaker.rb
+++ Casks/tal-noisemaker.rb
@@ -1,17 +1,19 @@
 cask "tal-noisemaker" do
-  version "5.0.4"
+  version "5.0.6"
   sha256 :no_check
 
-  url "https://tal-software.com//downloads/plugins/tal-noisemaker-installer.pkg"
+  url "https://tal-software.com//downloads/plugins/TAL-NoiseMaker-installer_macos.zip"
   name "TAL-NoiseMaker"
+  desc "Virtual analog synthesizer"
   homepage "https://tal-software.com/products/tal-noisemaker"
 
-  pkg "tal-noisemaker-installer.pkg"
+  pkg "TAL-NoiseMaker-installer.pkg"
 
   uninstall pkgutil: [
     "com.talsoftware.pkg.TAL-NoiseMaker-AAX",
-    "com.talsoftware.pkg.TAL-NoiseMaker-VST3",
-    "com.talsoftware.pkg.TAL-NoiseMaker-VST",
     "com.talsoftware.pkg.TAL-NoiseMaker-AU",
+    "com.talsoftware.pkg.TAL-NoiseMaker-CLAP",
+    "com.talsoftware.pkg.TAL-NoiseMaker-VST",
+    "com.talsoftware.pkg.TAL-NoiseMaker-VST3",
   ]
 end
