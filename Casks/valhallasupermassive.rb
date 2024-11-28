c0b2643a12e65e5ebf0672ad65b71829311d17a2
Juan Ignacio Donoso <jidonoso@gmail.com>
2024-11-27 22:43:19 -0300

    valhallasupermassive 4.0.0

--- Casks/valhallasupermassive.rb
+++ Casks/valhallasupermassive.rb
@@ -1,8 +1,8 @@
 cask "valhallasupermassive" do
-  version "3.0.0"
-  sha256 "a416c6bfb2e2383de30832c84a960bbbc6032c9cc8d747cd27086c658ddea6ce"
+  version "4.0.0"
+  sha256 "6f64b1401cc788877bc19ad07b1d52039196b268702fdd1f458388b2f9954377"
 
-  url "https://valhallaproduction.s3-us-west-2.amazonaws.com/supermassive/ValhallaSupermassiveOSX_#{version.dots_to_underscores}v3.dmg",
+  url "https://valhallaproduction.s3-us-west-2.amazonaws.com/supermassive/ValhallaSupermassiveOSX_#{version.dots_to_underscores}v4.dmg",
       verified: "valhallaproduction.s3-us-west-2.amazonaws.com/"
   name "Valhalla Supermassive"
   desc "Plugin for massive reverbs, harmonic echoes and space sounds"
