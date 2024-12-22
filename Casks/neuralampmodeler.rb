78c842899523a00e5753edd89e7007465a583f00
Hannes Braun <hannes@hannesbraun.net>
2024-12-23 00:12:17 +0100

    neuralampmodeler 0.7.13

--- Casks/neuralampmodeler.rb
+++ Casks/neuralampmodeler.rb
@@ -1,6 +1,6 @@
 cask "neuralampmodeler" do
-  version "0.7.12"
-  sha256 "284bbc02b29dc0e10cc9bab77de4f8eac6c0b181c403c4e5e29f6854165d3a14"
+  version "0.7.13"
+  sha256 "e9cea7a409fd167e05314d423a298ecb3b2c915eba2f8a514ee0c48a70f39173"
 
   url "https://github.com/sdatkinson/NeuralAmpModelerPlugin/releases/download/v#{version}/NeuralAmpModeler-v#{version}-mac.dmg"
   name "Neural Amp Modeler"
